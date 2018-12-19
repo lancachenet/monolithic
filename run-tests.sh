b09b1aa20ca5c24fd00be648e0301e5f235d7bd0
VibroAxe <vibs@macrolevel.co.uk>
2018-12-19 17:13:51 +0000

    Added circle ci integration
    Added circle ci integration and automatic testing

---
+++ run-tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+which goss
+
+if [ $? -ne 0 ]; then
+	echo "Please install goss from https://goss.rocks/install"
+	echo "For a quick auto install run the following"
+	echo "curl -fsSL https://goss.rocks/install | sh"
+	exit $?
+fi
+
+docker build --tag steamcache/monolithic:goss-test .
+case $1 in
+  circleci)
+    shift;
+    mkdir -p ./reports/goss
+	if [[ "$1" == "keepimage" ]]; then
+		KEEPIMAGE=true
+		shift
+	fi
+    export GOSS_OPTS="$GOSS_OPTS --format junit"
+	dgoss run $@ steamcache/monolithic:goss-test > reports/goss/report.xml
+	#store result for exit code
+	RESULT=$?
+	#delete the junk that goss currently outputs :(
+    sed -i '0,/^</d' reports/goss/report.xml
+	#remove invalid system-err outputs from junit output so circleci can read it
+	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
+    ;;
+  *)
+	if [[ "$1" == "keepimage" ]]; then
+		KEEPIMAGE=true
+		shift
+	fi
+	dgoss run $@ steamcache/monolithic:goss-test
+	RESULT=$?
+    ;;
+esac
+[[ "$KEEPIMAGE" == "true" ]] || docker rmi steamcache/monolithic:goss-test
+
+exit $RESULT
