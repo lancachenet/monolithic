42600bd00dd94cef18a574df42213d1b46ac3700
VibroAxe <vibs@macrolevel.co.uk>
2019-12-02 11:26:37 +0000

    Inverted deprecation check

--- overlay/hooks/entrypoint-pre.d/00_deprecation.sh
+++ overlay/hooks/entrypoint-pre.d/00_deprecation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z "${CACHE_DOMAIN_REPO}" ]]; then
+if [[ ! -z "${CACHE_DOMAIN_REPO}" ]]; then
 
 	echo "ERROR: CACHE_DOMAIN_REPO environment variable has be deprecated in favour of CACHE_DOMAINS_REPO. Please update your config"
 	exit 1
--- run-tests.sh
+++ run-tests.sh
@@ -8,6 +8,8 @@ if [ $? -ne 0 ]; then
 	exit $?
 fi
 
+GOSS_WAIT_OPS="-r 60s -s 1s"
+
 docker build --tag lancachenet/monolithic:goss-test .
 case $1 in
   circleci)
@@ -25,6 +27,26 @@ case $1 in
     sed -i '0,/^</d' reports/goss/report.xml
 	#remove invalid system-err outputs from junit output so circleci can read it
 	sed -i '/<system-err>.*<\/system-err>/d' reports/goss/report.xml
+    ;;
+  docker)
+	shift;
+	if [[ "$1" == "keepimage" ]]; then
+		KEEPIMAGE=true
+		shift
+	fi
+	docker run --name monolithic-goss-test $@ lancachenet/monolithic:goss-test
+	docker stop monolithic-goss-test
+	docker rm monolithic-goss-test
+	RESULT=$?
+	;;
+  edit)
+	shift;
+	if [[ "$1" == "keepimage" ]]; then
+		KEEPIMAGE=true
+		shift
+	fi
+	dgoss edit $@ lancachenet/monolithic:goss-test
+	RESULT=$?
     ;;
   *)
 	if [[ "$1" == "keepimage" ]]; then
