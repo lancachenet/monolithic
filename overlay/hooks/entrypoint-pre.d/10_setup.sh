2fa012f66742ccd129467722c113857941eb580e
VibroAxe <vibs@macrolevel.co.uk>
2021-10-09 10:24:38 +0000

    Ported CACHE_INDEX_SIZE PR from generic
    Ported PR https://github.com/lancachenet/generic/pull/103/ from @crabbey
    Changes CACHE_MEM_SIZE to CACHE_INDEX_SIZE but maintains backwards compatiblity through deprecation.sh

--- overlay/hooks/entrypoint-pre.d/10_setup.sh
+++ overlay/hooks/entrypoint-pre.d/10_setup.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
+# Handle CACHE_MEM_SIZE deprecation
+if [[ ! -z "${CACHE_MEM_SIZE}" ]]; then
+    CACHE_INDEX_SIZE=${CACHE_MEM_SIZE}
+fi
+
 # Preprocess UPSTREAM_DNS to allow for multiple resolvers using the same syntax as lancache-dns
 UPSTREAM_DNS="$(echo -n "${UPSTREAM_DNS}" | sed 's/[;]/ /g')"
 
 echo "worker_processes ${NGINX_WORKER_PROCESSES};" > /etc/nginx/workers.conf
 sed -i "s/^user .*/user ${WEBUSER};/" /etc/nginx/nginx.conf
-sed -i "s/CACHE_MEM_SIZE/${CACHE_MEM_SIZE}/"  /etc/nginx/conf.d/20_proxy_cache_path.conf
+sed -i "s/CACHE_INDEX_SIZE/${CACHE_INDEX_SIZE}/"  /etc/nginx/conf.d/20_proxy_cache_path.conf
 sed -i "s/CACHE_DISK_SIZE/${CACHE_DISK_SIZE}/" /etc/nginx/conf.d/20_proxy_cache_path.conf
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/cache.conf.d/root/20_cache.conf
 sed -i "s/slice 1m;/slice ${CACHE_SLICE_SIZE};/" /etc/nginx/sites-available/cache.conf.d/root/20_cache.conf
