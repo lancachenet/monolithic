9f9a8a7592b22b702e825b7273ad3fa668cea186
Markus Reiter <me@reitermark.us>
2022-02-23 17:32:52 +0100

    Update 00_deprecation.sh

--- overlay/hooks/entrypoint-pre.d/00_deprecation.sh
+++ overlay/hooks/entrypoint-pre.d/00_deprecation.sh
@@ -11,5 +11,4 @@ if [[ ! -z "${CACHE_MEM_SIZE}" ]]; then
 	echo " *** CACHE_MEM_SIZE has been deprecated in place of CACHE_INDEX_SIZE"
 	echo " *** Using CACHE_MEM_SIZE as the value"
 	echo " *** Please update your configuration at your earliest convenience"
-	CACHE_INDEX_SIZE=$CACHE_MEM_SIZE
 fi
