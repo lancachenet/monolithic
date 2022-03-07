4a09959e96cb17a3de31e6cbad7ab51de1fa5446
James Kinsman <VibroAxe@users.noreply.github.com>
2022-03-07 08:47:00 +0000

    Update 00_deprecation.sh
    Invert logic for cache_mem_size deprecation check

--- overlay/hooks/entrypoint-pre.d/00_deprecation.sh
+++ overlay/hooks/entrypoint-pre.d/00_deprecation.sh
@@ -7,7 +7,7 @@ if [[ ! -z "${CACHE_DOMAIN_REPO}" ]]; then
 
 fi
 
-if [[ ! -z "${CACHE_INDEX_SIZE}" ]]; then
+if [[ ! -z "${CACHE_MEM_SIZE}" ]]; then
 	echo " *** CACHE_MEM_SIZE has been deprecated in place of CACHE_INDEX_SIZE"
 	echo " *** Using CACHE_MEM_SIZE as the value"
 	echo " *** Please update your configuration at your earliest convenience"
