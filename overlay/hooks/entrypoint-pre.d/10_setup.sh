a45516468d9dd5dd7b96cdf311c07adb472c4e7f
James Kinsman <VibroAxe@users.noreply.github.com>
2022-11-12 12:10:20 +0000

    Addition of upstream proxy to resolve slice redirect errors (302 Playstation) (#156)
    Addition of Upstream proxy from #60 to test 302 cache impact on PS5
    caching
    Refactored upstream proxy into lancache standard structure.
    Simplified both location blocks into a single file rather than further
    delegated folders
    Fixed spacing & indents

--- overlay/hooks/entrypoint-pre.d/10_setup.sh
+++ overlay/hooks/entrypoint-pre.d/10_setup.sh
@@ -17,4 +17,5 @@ sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/" /etc/nginx/conf.d/20_proxy_cache_path
 sed -i "s/CACHE_MAX_AGE/${CACHE_MAX_AGE}/"    /etc/nginx/sites-available/cache.conf.d/root/20_cache.conf
 sed -i "s/slice 1m;/slice ${CACHE_SLICE_SIZE};/" /etc/nginx/sites-available/cache.conf.d/root/20_cache.conf
 sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/nginx/sites-available/cache.conf.d/10_root.conf
+sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/nginx/sites-available/upstream.conf.d/10_resolver.conf
 sed -i "s/UPSTREAM_DNS/${UPSTREAM_DNS}/"    /etc/nginx/stream-available/10_sni.conf
