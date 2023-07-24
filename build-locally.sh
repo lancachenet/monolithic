a8f810c7646f0b98f14ccf67acdd08b7b74589ba
VibroAxe <vibs@macrolevel.co.uk>
2023-07-24 08:27:36 +0000

    Fixed local build script
    Build source images instead of pull from docker hub
    Build monolithic from local repo

--- build-locally.sh
+++ build-locally.sh
@@ -3,30 +3,20 @@
 
 PURPLEBOLD="$(tput setf 5 bold)"
 
-#Checks if image ubuntu already exists:
-
-IMAGEEXISTS=true
-if [[ "$(docker image inspect ubuntu >/dev/null 2>&1 && echo true || echo false)" == "false" ]]; then
-  IMAGEEXISTS=false
-fi
-
 printf "${PURPLEBOLD}Building temporary modified Ubuntu image:\n"
 docker build -t lancachenet/ubuntu:latest --progress tty https://github.com/lancachenet/ubuntu.git
-
-#Removes standard Ubuntu image if not present before running:
-if [ "$IMAGEEXISTS" == false ]; then
-  printf "${PURPLEBOLD}Removing standard Ubuntu image:\n"
-  docker rmi ubuntu
-fi
 
 printf  "${PURPLEBOLD}Building temporary Ubuntu-Nginx image:\n"
 docker build -t lancachenet/ubuntu-nginx:latest --progress tty https://github.com/lancachenet/ubuntu-nginx.git
 
 printf "${PURPLEBOLD}Building Monolithic image:\n"
-docker build -t lancachenet/monolithic:latest --progress tty https://github.com/lancachenet/monolithic.git
+docker build -t lancachenet/monolithic:latest --progress tty .
 
 printf "${PURPLEBOLD}Removing temporary Ubuntu image:\n"
 docker rmi lancachenet/ubuntu
 
 printf "${PURPLEBOLD}Removing temporary Ubuntu-Nginx image:\n"
 docker rmi lancachenet/ubuntu-nginx
+
+printf "${PURPLEBOLD}Completed local build. Image now available as lancachenet/monolithic:latest\n"
+docker image ls lancachenet/monolithic:latest
