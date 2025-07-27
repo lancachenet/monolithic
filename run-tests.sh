9edba21e966ac07892b86a3e96621458f1452eb1
Amir Zarrinkafsh <nightah@me.com>
2025-07-27 19:05:26 +1000

    fix: copy pasta image mismatch

--- run-tests.sh
+++ run-tests.sh
@@ -6,4 +6,4 @@ for arg in "$@"; do
 	[[ "${arg}" == "--" ]] && SD_LOGLEVEL=(-e SUPERVISORD_LOGLEVEL=INFO) && break
 done
 
-curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/ubuntu:goss-test" "$@" "${SD_LOGLEVEL[@]}"
+curl -fsSL https://raw.githubusercontent.com/lancachenet/test-suite/master/dgoss-tests.sh | bash -s -- --imagename="lancachenet/monolithic:goss-test" "$@" "${SD_LOGLEVEL[@]}"
