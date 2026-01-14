a33e23b1d90c63e275955519251cedddb59db9a5
James Kinsman <1069212+VibroAxe@users.noreply.github.com>
2026-01-14 23:19:18 +0000

    Added diagnostics to cache_test.sh

--- overlay/scripts/cache_test.sh
+++ overlay/scripts/cache_test.sh
@@ -9,9 +9,16 @@ sleep 5
 pageload4=`curl http://worldtimeapi.org/api/timezone/ETC/GMT --resolve worldtimeapi.org:80:127.0.0.1`
 if [ "$pageload1" == "$pageload2" ]; then
 	if [ "$pageload3" == "$pageload4" ]; then
-		if [ "$pageload1" == "$pageload4" ]; then
+		if [ "$pageload1" == "$pageload3" ]; then
 			#In monolithic pages 1+3 should be different as there is no map for this test case
 			echo "Error caching test page, pages 1+3 are identical"
+	
+			echo "pageload1:"
+			echo $pageload1
+
+			echo "pageload3:"
+			echo $pageload3
+			
 			exit -3
 		else
 			echo "Succesfully Cached"
@@ -20,10 +27,26 @@ if [ "$pageload1" == "$pageload2" ]; then
 
 	else
 		echo "Error caching test page, pages 3+4 differed"
+
+	
+			echo "pageload3:"
+			echo $pageload3
+
+			echo "pageload4:"
+			echo $pageload4
+		
 		exit -2
 	fi
 
 else
 	echo "Error caching test page, pages 1+2 differed"
+
+
+	echo "pageload1:"
+	echo $pageload1
+
+	echo "pageload2:"
+	echo $pageload2
+	
 	exit -1
 fi
