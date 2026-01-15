d056fe672d6374bba7120f30787ef2de1c614863
James Kinsman <1069212+VibroAxe@users.noreply.github.com>
2026-01-15 13:22:13 +0000

    Fix tab indentation

--- overlay/hooks/supervisord-pre.d/99_config_check.sh
+++ overlay/hooks/supervisord-pre.d/99_config_check.sh
@@ -11,7 +11,7 @@ echo "Checking nginx config"
 echo "Ready for supervisord startup"
 if [ -n "$CACHE_ROOT" ]
 then
-     echo "Monitor ${CACHE_ROOT}/logs/access.log and ${CACHE_ROOT}/logs/error.log on the host for cache activity"
+    echo "Monitor ${CACHE_ROOT}/logs/access.log and ${CACHE_ROOT}/logs/error.log on the host for cache activity"
 else
     echo "Monitor access.log and error.log on the host for cache activity"
 fi
