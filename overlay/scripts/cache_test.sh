2f9f00b6e36576e1dfde1c10065464848930f15c
James Kinsman <1069212+VibroAxe@users.noreply.github.com>
2026-01-16 11:20:18 +0000

    Update cache test script for new endpoints and error codes

--- overlay/scripts/cache_test.sh
+++ overlay/scripts/cache_test.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 set -e
-pageload1=`curl http://www.worldtimeapi.org/api/timezone/ETC/GMT --resolve www.worldtimeapi.org:80:127.0.0.1`
-sleep 5
-pageload2=`curl http://www.worldtimeapi.org/api/timezone/ETC/GMT --resolve www.worldtimeapi.org:80:127.0.0.1`
-sleep 5
-pageload3=`curl http://worldtimeapi.org/api/timezone/ETC/GMT --resolve worldtimeapi.org:80:127.0.0.1`
-sleep 5
-pageload4=`curl http://worldtimeapi.org/api/timezone/ETC/GMT --resolve worldtimeapi.org:80:127.0.0.1`
+pageload1=`curl http://ping1.lancache.net/api/timezone/ETC/GMT --resolve ping1.lancache.net:80:127.0.0.1`
+sleep 2
+pageload2=`curl http://ping1.lancache.net/api/timezone/ETC/GMT --resolve ping1.lancache.net:80:127.0.0.1`
+sleep 2
+pageload3=`curl http://ping2.lancache.net/api/timezone/ETC/GMT --resolve ping2.lancache.net:80:127.0.0.1`
+sleep 2
+pageload4=`curl http://ping2.lancache.net/api/timezone/ETC/GMT --resolve ping2.lancache.net:80:127.0.0.1`
+
 if [ "$pageload1" == "$pageload2" ]; then
 	if [ "$pageload3" == "$pageload4" ]; then
 		if [ "$pageload1" == "$pageload3" ]; then
 			#In monolithic pages 1+3 should be different as there is no map for this test case
 			echo "Error caching test page, pages 1+3 are identical"
-	
+
 			echo "pageload1:"
 			echo $pageload1
 
 			echo "pageload3:"
 			echo $pageload3
-			
-			exit -3
+
+			exit 3
 		else
 			echo "Succesfully Cached"
 			exit 0
@@ -28,14 +29,14 @@ if [ "$pageload1" == "$pageload2" ]; then
 	else
 		echo "Error caching test page, pages 3+4 differed"
 
-	
-			echo "pageload3:"
-			echo $pageload3
 
-			echo "pageload4:"
-			echo $pageload4
-		
-		exit -2
+		echo "pageload3:"
+		echo $pageload3
+
+		echo "pageload4:"
+		echo $pageload4
+
+		exit 2
 	fi
 
 else
@@ -47,6 +48,6 @@ else
 
 	echo "pageload2:"
 	echo $pageload2
-	
-	exit -1
+
+	exit 1
 fi
